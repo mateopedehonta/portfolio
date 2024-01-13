153c66b6b3b13983634e94f3a026f554de112525
mateopedehonta <mateopedehonta@gmail.com>
2024-01-13 15:58:18 -0300

    ItemProyects

--- src/components/proyects/ItemProyects.tsx
+++ src/components/proyects/ItemProyects.tsx
@@ -3,6 +3,8 @@ import { Proyect } from "../../interfaces/Proyect.interfaces";
 import { BiLogoTypescript } from "react-icons/bi";
 import { FaExternalLinkAlt, FaReact } from "react-icons/fa";
 import { BsGithub } from "react-icons/bs";
+import { IoLogoVercel } from "react-icons/io5";
+import { Icon } from '@iconify/react';
 
 export const ItemProyects = ({
   description,
@@ -10,27 +12,47 @@ export const ItemProyects = ({
   link,
   name,
   stack,
+  deploy,
+  github,
 }: Proyect) => {
   return (
-    <div className="w-full bg-1 flex flex-col  rounded-lg ">
-      <a target="_blank" className="min-h-[190px] text-black hover:text-blue-600" href={link}>
+    <div className="w-full bg-1 flex flex-col  rounded-lg overflow-hidden">
+      <a
+        target="_blank"
+        className="max-h-[190px] relative text-black hover:text-blue-600 "
+        href={link}
+      >
         <FaExternalLinkAlt className="absolute right-0 m-2 " />
 
-        <img className="w-full h-full " src={img} alt="" />
+        <img className="w-full h-full object-cover " src={img} alt="" />
       </a>
-      <div className="h-full flex flex-col justify-between py-2 px-4 gap-2">
-        <h1 className="text-2xl capitalize">{name}</h1>
-        <p>{description}</p>
-        <div className="text-2xl  flex justify-between">
-          <div className="flex items-center gap-4 text-xl">
-            <p className="flex text-xl gap-3">
-              <FaReact className=" hover:text-blue-700" />
-              <BiLogoTypescript className=" hover:text-blue-700" />
-            </p>
-
-            <p className="text-sm hover:text-blue-700">{stack}</p>
+      <div className="h-full flex flex-col justify-between  py-2 px-4 gap-5">
+        <div className="flex flex-col gap-3">
+          <div className="flex w-full justify-between">
+            <h1 className="text-2xl capitalize">{name}</h1>
+            <span className="flex items-center gap-2 text-sm">
+              {deploy}
+              {deploy === 'vercel' && <IoLogoVercel className="bg-black px-[6px] text-2xl rounded-full" />}
+              {deploy === 'netlify' && <Icon className="text-2xl" icon="vscode-icons:file-type-netlify" />
+}
+            </span>
           </div>
-          <BsGithub className=" hover:text-blue-700" />
+          <a
+            className="flex items-center gap-2 font-sans bg-black w-max py-1 px-3 rounded-3xl hover:underline"
+            href={`https://github.com/mateopedehonta/${github}`}
+            target="_blank"
+          >
+            <BsGithub className=" hover:text-blue-700" />
+            <p>{`mateopedehonta/${github}`}</p>
+          </a>
+        </div>
+        <p>{description}</p>
+        <div className="  flex items-center  gap-4">
+          <p className="flex text-xl gap-3">
+            <FaReact className=" hover:text-blue-700" />
+            <BiLogoTypescript className=" hover:text-blue-700" />
+          </p>
+          <p className="text-sm hover:text-blue-700">{stack}</p>
         </div>
       </div>
     </div>
